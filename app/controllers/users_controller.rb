212226e99c091fd9ea6d28afacee1e8f24685c7d
chienleow <chienleow1@gmail.com>
2020-08-07 22:09:58 -0400

    login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,10 @@ class UsersController < ApplicationController
 
     # POST /users - Create the User Action
     def create
+        @user = User.new
+        @user.email = params[:email]
+        @user.save
+
+        redirect_to "/login"
     end
 end
