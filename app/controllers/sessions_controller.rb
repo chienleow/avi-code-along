4ba281fcf220f558c3801217b8db912366092513
chienleow <chienleow1@gmail.com>
2020-10-02 22:27:11 -0400

    Add user create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(:email => params[:email])
+        if @user
+            session[:current_user_id] = @user.id
+            # save the user into the session so across all the different pages you are on, you can access the user id
+            redirect_to "/"
+        else
+            redirect_to "/login"
+        end
     end
 end
