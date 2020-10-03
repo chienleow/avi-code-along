8dd6b6f4248ba2c8c9007ed159df43d7f18063bc
chienleow <chienleow1@gmail.com>
2020-10-02 22:37:39 -0400

    Build login(user) helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
             @current_user = User.find(session[:current_user_id])
         end
     end
+
+    def login(user)
+        session[:current_user_id] = @user.id
+    end
 end
