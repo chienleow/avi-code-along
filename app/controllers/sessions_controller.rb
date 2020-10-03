b7c6ec40a09b134c6bbc496e8d2ee2e1fcfc8a85
chienleow <chienleow1@gmail.com>
2020-10-02 22:36:37 -0400

    Build current_user helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(:email => params[:email])
         if @user
-            session[:current_user_id] = @user.id
+            login(@user)
             # save the user into the session so across all the different pages you are on, you can access the user id
             redirect_to "/"
         else
